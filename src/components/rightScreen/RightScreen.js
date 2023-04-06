6abd7b2aff3e768b76fdc5250fe6c59a5db927e9
UgyenDorji129 <ugyendora@gmail.com>
2023-04-07 02:27:19 +0530

    display

--- src/components/rightScreen/RightScreen.js
+++ src/components/rightScreen/RightScreen.js
@@ -16,7 +16,7 @@ const RightScreen = () => {
 
   return (
   
-      <Box sx={{height:{xs:"100vh", md:"75vh"}, overflow:"auto", width:"100%"}}>
+      <Box sx={{height: "-webkit-fill-available", height:{xs:"100vh", md:"75vh"}, overflow:"auto", width:"100%"}}>
       <Routes>
         <Route exact path={"/"} element={<HomeScreen/>}/>
         <Route exact path={"/about"} element={<AboutScreen/>}/>
