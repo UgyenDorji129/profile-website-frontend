9eedbc7f1e55187bada045a11b2f10cbabb7c785
UgyenDorji129 <ugyendora@gmail.com>
2023-04-07 01:31:46 +0530

    mobile

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ function App() {
     <ThemeProvider theme={theme}>
       <Router>
         <Box maxWidth={"xl"} sx={{background: {xs:"white",md:"#e3f2fd"},  height:"100vh", display:"flex", alignItems:{xs:"center", md:"center"}, overflow:"hidden"}}>
-          <Grid container sx={{justifyContent:"space-evenly", alignItems:"stretch"}}>
+          <Grid container sx={{justifyContent:"space-evenly", alignItems:"stretch",maxHeight:{md:"80%"}, alignContent:"center"}}>
             <Grid item container xs ={11} md={10}  sx={{borderRadius:"24px",bgcolor:{md:theme.palette.primary.main,xs:"white", display:{sx:"flex", md:"block"}, justifyContent:{sx:"center"}}}} >
                 <Grid item md={3} sx={{display:{md:"block", xs:"none"},}}><LeftScreen /></Grid>
                 <Grid item xs={12} md={9} sx={{borderRadius:"24px", background:"white", display: "flex", justifyContent:"center", alignItems:"center", }}><RightScreen/></Grid>
             </Grid>
             <Grid item   md={1}  sx={{display:{xs:"none", md:"block"}, borderRadius:"24px", bgcolor:"white", color:theme.palette.primary.main}}><DesktopMenu/></Grid>
-            <Grid item   xs={1}  sx={{display:{xs:"block", md:"none"}, borderRadius:"24px", bgcolor:"white", color:theme.palette.primary.main, height:"100vh"}}><MobileMenu/></Grid>
+            <Grid item   xs={1}  sx={{display:{xs:"block", md:"none"}, borderRadius:"24px", bgcolor:"white", color:theme.palette.primary.main}}><MobileMenu/></Grid>
           </Grid>
         </Box>
       </Router>
