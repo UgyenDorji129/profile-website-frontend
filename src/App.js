9fbb1e8c0f060c47d298401a451d8913592a5643
UgyenDorji129 <ugyendora@gmail.com>
2023-04-07 09:25:21 +0530

    droba

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Box maxWidth={"xl"} sx={{background: {xs:"white",md:"#e3f2fd"},  position:'absolute',top:'0px',right:'0px',bottom:'0px',
+        <Box  sx={{background: {xs:"white",md:"#e3f2fd"},  position:'absolute',top:'0px',right:'0px',bottom:'0px',
           left:'0px',overflow:"hidden",}}>
-          <Grid container sx={{justifyContent:"space-evenly", alignItems:"stretch",height:"100%", alignContent:"center"}}>
-            <Grid item container xs ={11} md={10}  sx={{borderRadius:"24px",bgcolor:{md:theme.palette.primary.main,xs:"white", display:{sx:"flex", md:"block"}, justifyContent:"center"}}} >
+          <Grid maxWidth={"xl"} container sx={{justifyContent:"space-evenly", alignItems:{md:"stretch"},height:"100%", alignContent:"center"}}>
+            <Grid item container xs ={11} md={10}  sx={{borderRadius:"24px",bgcolor:{md:theme.palette.primary.main,xs:"white"}, display:"flex", justifyContent:"center"}} >
                 <Grid item md={3} sx={{display:{md:"block", xs:"none"},}}><LeftScreen /></Grid>
-                <Grid item xs={12} md={9} sx={{borderRadius:"24px", background:"white", display: "flex", justifyContent:"center", alignItems:"center", }}><RightScreen/></Grid>
+                <Grid item xs={12} md={9} sx={{borderRadius:"24px", background:"white", display: "flex", justifyContent:"center", alignItems:"center"}}><RightScreen/></Grid>
             </Grid>
             <Grid item   md={1}  sx={{display:{xs:"none", md:"block"}, borderRadius:"24px", bgcolor:"white", color:theme.palette.primary.main}}><DesktopMenu/></Grid>
             <Grid item   xs={1}  sx={{display:{xs:"block", md:"none"}, borderRadius:"24px", bgcolor:"white", color:theme.palette.primary.main}}><MobileMenu/></Grid>
--- src/components/rightScreen/RightScreen.js
+++ src/components/rightScreen/RightScreen.js
@@ -16,7 +16,7 @@ const RightScreen = () => {
 
   return (
   
-      <Box sx={{height:{xs:"100svh", md:"75vh"}, overflow:"auto", padding:"10px"}}>
+      <Box sx={{height:{xs:"100svh", md:"75svh"}, overflow:"auto", padding:"10px"}}>
       <Routes>
         <Route exact path={"/"} element={<HomeScreen/>}/>
         <Route exact path={"/about"} element={<AboutScreen/>}/>
